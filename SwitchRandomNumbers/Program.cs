7caed4edc52ba7c4c973a89ceff4768e8ea20f60
Opilane TTHK <opilane@tthk.ee>
2025-10-16 11:33:33 +0300

    SwitchRandomNumbers

---
+++ SwitchRandomNumbers/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace SwitchRandomNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sisesta numbrid");
+
+            //genereerib iga kord suvalised numbrid
+            //1-st kuni 6-ni
+            
+            //pete kasutama switchi, et igat numbrit näidata
+;
+            int number = new Random().Next(1,6);
+
+            switch (number)
+            {
+                case 1:
+                    Console.WriteLine("Sistestasid number 1");
+                    break;
+
+                case 2:
+                    Console.WriteLine("Sistestasid number 2");
+                    break;
+
+                case 3:
+                    Console.WriteLine("Sistestasid number 3");
+                    break;
+
+                case 4:
+                    Console.WriteLine("Sistestasid number 4");
+                    break;
+
+                case 5:
+                    Console.WriteLine("Sistestasid number 5");
+                    break;
+
+                case 6:
+                    Console.WriteLine("Sistestasid number 6");
+                    break;
+
+                default:
+                    Console.WriteLine("ERROR");
+                    break;
+            }
+        }
+    }
+}
